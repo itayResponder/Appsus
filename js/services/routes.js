a9590de964d5e2b09e908d5b8e5e110405bd949e
Itay Azulay <itay.responder@gmail.com>
2019-06-20 12:24:24 +0300

    test

--- js/services/routes.js
+++ js/services/routes.js
@@ -1,4 +1,4 @@
-// import emailApp from '../pages/email/email-app.cmp.js';
+import emailApp from '../pages/email/email-app.cmp.js';
 // import bookApp from './pages/book-app.cmp.js';
 // import aboutCmp from './pages/about.cmp.js';
 // import bookDetails from './pages/book-details.cmp.js';
@@ -6,7 +6,7 @@
 
 export default [
     // { path: '/', component: mainHeader },
-    // { path: '/miss-email', component: emailApp },
+    { path: '/miss-email', component: emailApp },
     // { path: '/books', component: bookApp },
     // { path: '/book/:bookId', component: bookDetails },
     // { path: '/addBook', component: addBook },
