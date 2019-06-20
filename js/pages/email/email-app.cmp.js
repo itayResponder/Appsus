755c24120a88ca510f35db1c5c9221c5d07141ea
Oriel Shalem <orielshalem@gmail.com>
2019-06-20 12:20:30 +0300

    email app

--- js/pages/email/email-app.cmp.js
+++ js/pages/email/email-app.cmp.js
@@ -2,4 +2,43 @@
 // • <email-app>
 // • EmailApp Gets emails from service (asynch)t= <teas></teas>
 
-import { bookService } from '../services/book.service.js'
+import { emailService } from '../../services/email/email.service.js'
+import emailList from '../../cmps/email/email-list.cmp.js'
+
+export default {
+    template: `
+    <section class="email-app">
+        <h1>Email App</h1>
+        <!-- <email-filter @set-filter="setFilter"></email-filter> -->
+        <email-list :emails="emailsForDisplay" @selected="onSelected"></email-list>
+        <!-- <email-details v-if="selectedemail" :email="selectedemail" @close="selectedemail=null"></email-details>  -->
+    </section>
+    `,
+
+    data() {
+        return {
+            emails: ''
+        }
+    },
+
+    created() {
+        // V an array of books, on created – get the books from the service.
+        emailService.query()
+            .then(emails => this.emails = emails)
+    },
+
+    computed: {
+        booksForDisplay() {
+            if (!this.filter) return this.books;
+            // return this.books.filter(book => book.title.includes(this.filter.txt) &&
+            //     book.listPrice.amount > this.filter.fromNum &&
+            //     book.listPrice.amount < this.filter.toNum)
+        }
+    },
+
+    components: {
+        emailList,
+        emailDetails,
+        emailFilter
+    },
+}
---
+++ js/services/email/email.service.js
@@ -0,0 +1,84 @@
+import { storageService } from './storage.service.js'
+import { utilService } from './util.service.js'
+
+
+const EMAIL_KEY = 'email';
+
+var emailDB = [];
+
+export const emailService = {
+    query
+}
+
+
+function query() {
+    const email = storageService.load(EMAIL_KEY);
+    if (!email) {
+        email = getEmailData();
+        storageService.store(EMAIL_KEY, email)
+    }
+    emailDB = email;
+    console.log(emailDB)
+    return Promise.resolve(emailDB);
+}
+
+/* email containes:
+id
+from
+Date
+subject
+desc
+ */
+
+function getEmailData() {
+    return [
+        {
+            'id': 'OXeMG8wNskc',
+            'from': {
+                'name':'itay',
+                'thumbnail': ''
+            },
+            'Date': Date.now(),
+            'massage': {
+                'subject': 'My problems',
+                'desc': 'working with oriel'
+            }
+        },
+        {
+            'id': 'OXeMG8wNskc',
+            'from': {
+                'name':'itay',
+                'thumbnail': ''
+            },
+            'Date': Date.now(),
+            'massage': {
+                'subject': 'My problems',
+                'desc': 'working with oriel'
+            }
+        },
+        {
+            'id': 'OXeMG8wNskc',
+            'from': {
+                'name':'itay',
+                'thumbnail': ''
+            },
+            'Date': Date.now(),
+            'massage': {
+                'subject': 'My problems',
+                'desc': 'working with oriel'
+            }
+        },
+        {
+            'id': 'OXeMG8wNskc',
+            'from': {
+                'name':'itay',
+                'thumbnail': ''
+            },
+            'Date': Date.now(),
+            'massage': {
+                'subject': 'My problems',
+                'desc': 'working with oriel'
+            }
+        },
+    ]
+}
