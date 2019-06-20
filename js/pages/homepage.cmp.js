9bd204e65331c8eca28ce7cff6b9f1cad2661790
Itay Azulay <itay.responder@gmail.com>
2019-06-20 12:56:15 +0300

    css

---
+++ js/pages/homepage.cmp.js
@@ -0,0 +1,15 @@
+
+
+export default {
+    template: `
+    <section class="home">
+        <h1>Home Sweet Home</h1>
+        <div class= "apps-previews">
+            <div class="mail-preview"></div>
+            <div class="notes-preview"></div>
+            <h1>TEST DIV</h1>
+        </div>
+    </section>
+    `
+}
+
--- js/services/routes.js
+++ js/services/routes.js
@@ -1,11 +1,11 @@
+import homePage from '../pages/homepage.cmp.js';
 import emailApp from '../pages/email/email-app.cmp.js';
-// import bookApp from './pages/book-app.cmp.js';
 // import aboutCmp from './pages/about.cmp.js';
 // import bookDetails from './pages/book-details.cmp.js';
 // import addBook from './pages/book-add.cmp.js';
 
 export default [
-    // { path: '/', component: mainHeader },
+    { path: '/', component: homePage },
     { path: '/miss-email', component: emailApp },
     // { path: '/books', component: bookApp },
     // { path: '/book/:bookId', component: bookDetails },
