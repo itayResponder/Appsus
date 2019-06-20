02e0dc507a746402235846ae483c056a0bffa6ab
Oriel Shalem <orielshalem@gmail.com>
2019-06-20 12:27:34 +0300

    nothing

--- js/cmps/email/email-list.cmp.js
+++ js/cmps/email/email-list.cmp.js
@@ -1,20 +1,20 @@
-// import bookPreview from './book-preview.cmp.js';
+import emailPreview from './email-preview.cmp.js';
 
 {/* <email-list> renders a list of <email-preview> pass down an email prop */}
 
-// export default {
-//     template: `
-//     <section>
-//             <h1>Books</h1>
-//             <section class="books-list">
-//                 <book-preview v-for="currentbook in books" v-bind:book="currentbook">
-//                 </book-preview>
-//             </section>
-//         </section>
-//     `,
-//     props: ['books'],
+export default {
+    template: `
+    <section>
+            <h1>emails</h1>
+            <section class="emails-list">
+                <email-preview v-for="currentemail in emails" v-bind:email="currentemail">
+                </email-preview>
+            </section>
+        </section>
+    `,
+    props: ['emails'],
     
-//     components: {
-//         bookPreview
-//     }
-// }
+    components: {
+        emailPreview
+    }
+}
