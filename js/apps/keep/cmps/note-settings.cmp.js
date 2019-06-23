d95f9a44e311e0b14eb0b7e2c629774f91140fe9
Itay Azulay <itay.responder@gmail.com>
2019-06-23 18:12:27 +0300

    settings

--- js/apps/keep/cmps/note-settings.cmp.js
+++ js/apps/keep/cmps/note-settings.cmp.js
@@ -3,18 +3,23 @@ import { noteService } from '../services/notes-service.js'
 export default {
 
     template: `
-        <section class="settings">
+        <div class="settings">
             <div>
-                <img @click.stop="color" src="../../../../svg/color-plate.svg" :class="{colorClicked: this.isColorClicked}"/>
-                <img @click.stop="deleteNote" src="../../../../svg/pin.svg" :class="{}"/>
-                <img @click.stop="deleteNote" src="../../../../svg/trash-can.svg" :class="{}"/>
+                <textarea v-model.onchange="txt"></textarea>
+                <img @click.stop="noteTxt" src="../../../../svg/text.svg"/>
+                <img @click.stop="noteImage" src="../../../../svg/image.svg"/>
+                <img @click.stop="noteTodo" src="../../../../svg/todo.svg"/>
+                <img @click.stop="colorNote" src="../../../../svg/color-plate.svg" :class="{colorClicked: this.isColorClicked}"/>
+                <img @click.stop="pinNote" src="../../../../svg/pin.svg"/>
+                <img @click.stop="deleteNote" src="../../../../svg/trash-can.svg"/>
             </div>
-        </section>
+        </div>
         
         `,
 
         data() {
             return {
+                txt:'',
                 isColorClicked: false,
                 colors: []
             }
@@ -28,8 +33,21 @@ export default {
         },
 
         methods: {
-            color() {
+            colorNote() {
                 this.isColorClicked = !this.isColorClicked;
+            },
+            noteTxt() {
+                noteService.add('txt',this.txt);
+                this.txt = '';
+            },
+            noteImage() {
+                noteService.add('img',this.txt);
+                this.txt = '';
+            },
+    
+            noteTodo() {
+                noteService.add('todo',this.txt);
+                this.txt = '';
             },
         }
     
--- js/apps/keep/pages/notes-app.cmp.js
+++ js/apps/keep/pages/notes-app.cmp.js
@@ -1,22 +1,12 @@
 import { noteService } from '../services/notes-service.js'
 
-import todoList from '../cmps/note-todo.cmp.js'
 import noteList from '../cmps/note-list.cmp.js'
-
+import noteSetting from '../cmps/note-settings.cmp.js';
 export default {
     template: `
         <section class="notes-app">
-            <h1>Notes-App</h1>
                 <div class="note-start">
-                    <textarea v-model.onchange="txt"></textarea>
-                    <div class="settings">
-                        <img @click.stop="noteTxt" src="../../../../svg/text.svg"/>
-                        <img @click.stop="noteImage" src="../../../../svg/image.svg"/>
-                        <img @click.stop="noteTodo" src="../../../../svg/todo.svg"/>
-                        <img @click.stop="colorNote" src="../../../../svg/color-plate.svg" :class="{colorClicked: this.isColorClicked}"/>
-                        <img @click.stop="pinNote" src="../../../../svg/pin.svg"/>
-                        <img @click.stop="deleteNote" src="../../../../svg/trash-can.svg"/>
-                    </div>
+                    <note-setting></note-setting>
                 </div>      
             <note-list :notes = "notes"></note-list>
         </section>
@@ -24,9 +14,7 @@ export default {
 
     data() {
         return {
-            txt:'',
             notes: '',
-            isColorClicked: false,
         }
     },
 
@@ -36,26 +24,10 @@ export default {
     },
 
     components: {
-        todoList,
-        noteList
+        noteList,
+        noteSetting
     },
     methods: {
-        colorNote() {
-            this.isColorClicked = !this.isColorClicked;
-        },
-        noteTxt() {
-            noteService.add('txt',this.txt);
-            this.txt = '';
-        },
-        noteImage() {
-            noteService.add('img',this.txt);
-            this.txt = '';
-        },
-
-        noteTodo() {
-            noteService.add('todo',this.txt);
-            this.txt = '';
-        },
     }
 
 }
