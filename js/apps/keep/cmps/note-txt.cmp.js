2320021c578a4284c667bd481d0fb29ab6c4be77
Itay Azulay <itay.responder@gmail.com>
2019-06-23 14:33:51 +0300

    text

--- js/apps/keep/cmps/note-txt.cmp.js
+++ js/apps/keep/cmps/note-txt.cmp.js
@@ -4,7 +4,7 @@ export default {
     template: `
     <section class="note">
         <div class="note-txt">
-            <textarea v-model="text"></textarea>
+            <textarea v-model.lazy="text"></textarea>
             <pre>{{this.text}}</pre>
             <note-settings></note-settings>
         </div>
