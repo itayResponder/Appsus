e5ad5de527c6c3320006e7f09de8791148a49ec0
Itay Azulay <itay.responder@gmail.com>
2019-06-23 11:12:57 +0300

    asdasd

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -1,11 +1,13 @@
 import emailPreview from './email-preview.cmp.js';
+import emailStarred from './email-starred.cmp.js';
 {/* <email-list> renders a list of <email-preview> pass down an email prop */}
 
 export default {
     template: `
     <section class="emails-list">
             <section>
-                <email-preview v-for="currentemail,idx in emails" :key="idx" v-bind:email="currentemail">
+                <email-preview v-for="currentEmail,idx in emails" :key="idx" :email="currentEmail">
+                <email-starred v-if="!currentEmail.message.isStarred"></email-starred>
                 </email-preview>
             </section>
         </section>
@@ -15,5 +17,6 @@ export default {
     },
     components: {
         emailPreview,
+        emailStarred
     }
 }
--- js/apps/email/cmps/email-side-navbar.cmp.js
+++ js/apps/email/cmps/email-side-navbar.cmp.js
@@ -8,7 +8,7 @@ export default {
                     <li><router-link exact to="/miss-email/inbox">Inbox <unread></unread></router-link></li>
                     <li><router-link to="/miss-email/starred">Starred</router-link></li>
                     <li><router-link to="/miss-email/sent">Sent</router-link></li>
-                    <li><router-link to="/miss-email/trash">Trash</router-link></li> 
+                    
                 </ul>
             </nav>
         </div>    
