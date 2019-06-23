3f05619e9becba97ef83c36cd46f1106ef35dc50
Oriel Shalem <orielshalem@gmail.com>
2019-06-23 19:47:38 +0300

    cxv

--- js/apps/keep/cmps/note-preview.cmp.js
+++ js/apps/keep/cmps/note-preview.cmp.js
@@ -5,7 +5,7 @@ export default {
     <section class="note-preview" :style = "{backgroundColor: this.note.bgc}">
         <div class="note-container">
             <p v-if="note.txt"> {{note.txt}} </p>
-            <img v-else-if="note.img" :src="note.img" alt="" width="200px" />
+            <img v-else-if="note.img" :src="note.img" alt="" width="100%" />
             <ul v-else-if="note.list">
                 <li v-for='list in note.list'>{{list}}</li>
             </ul>
@@ -14,8 +14,8 @@ export default {
             <div class="input-color-container">
                 <input id="input-color" v-model='styleObject.backgroundColor' @change="colorPicked" class="input-color" type="color">
             </div>
-            <img @click.stop="pinNote"v-if="!note.pinned" src="../../../../svg/pin.svg"/>
-            <img @click.stop="pinNote"v-else src="../../../../svg/pinned.svg"/>
+            <img @click.stop="pinNote" v-if="!note.isPinned" src="../../../../svg/pin.svg"/>
+            <img @click.stop="pinNote" v-else src="../../../../svg/pinned.svg"/>
             <img @click.stop="deleteNote" src="../../../../svg/trash-can.svg"/>
         </div>
     </section>
